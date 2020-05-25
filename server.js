23b07487e17f7f4ba11dffdf97f383ebd97a7d72
Brent F <feuerbacherb@gmail.com>
2020-05-25 18:06:19 -0500

    added param by id get function

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 const { animals } = require('./data/animals');
 
 
+function findById(id, animalsArray) {
+   const result = animalsArray.filter(animal => animal.id === id)[0];
+   return result;
+}
+
 function filterByQuery(query, animalsArray) {
    let personalityTraitsArray = [];
    // Note that we save the animalsArray as filteredResults here:
@@ -49,7 +54,16 @@ app.get('/api/animals', (req, res) => {
       results = filterByQuery(req.query, results);
    }
    res.json(results);
-})
+});
+
+app.get('/api/animals/:id', (req, res) => {
+   const result = findById(req.params.id, animals);
+   if (result) {
+      res.json(result);
+   } else {
+      res.sendStatus(404);
+   }
+});
 
 app.listen(PORT, () => {
    console.log(`API server now on port ${PORT}!`);
